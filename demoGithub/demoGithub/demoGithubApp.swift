8a1cc621139bf071739ccd7bf3992d429e4e5664
ThanapoomPuttarn <rdrdkoo321@gmail.com>
2024-06-22 20:11:19 -0700

    initial commit

---
+++ demoGithub/demoGithub/demoGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGithubApp.swift
+//  demoGithub
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
